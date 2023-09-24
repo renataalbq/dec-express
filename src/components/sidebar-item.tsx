a4b77059fddb642a7ce2fd6b38f33f99d25e6776
Renata Albuquerque <renataalbq1@gmail.com>
2023-09-24 11:25:50 -0300

    create interfaces

--- src/components/sidebar-item.tsx
+++ src/components/sidebar-item.tsx
@@ -1,10 +1,16 @@
-const SidebarItem = ({icon, text, active})  => {
+interface SidebarItemProps {
+    icon?: any
+    text: string
+    active: boolean
+}
+
+const SidebarItem = (props: SidebarItemProps)  => {
     return (
         <li className={`relative flex items-center py-2 my-1 font-medium cursor-pointer
-        transition-colors ${active ? "bg-gradient-to-tr from-gray-600 to-gray-700 text-gray-100" :
+        transition-colors ${props.active ? "bg-gradient-to-tr from-gray-600 to-gray-700 text-gray-100" :
         "hover:bg-gray-700 text-gray-500"}`}>
-            {icon}
-            <span className="w-52 ml-3">{text}</span>
+            {props.icon && props.icon}
+            <span className="w-52 ml-3">{props.text}</span>
         </li>
     )
 }
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,11 +1,17 @@
-const Sidebar = ({children})  => {
+import { ReactNode } from "react"
+
+interface SidebarProps {
+    children: ReactNode
+}
+
+const Sidebar = (props: SidebarProps)  => {
     return (
         <aside className="h-screen">
             <nav className="h-full flex flex-col bg-black border-r shadow-sm">
                 <div className="p-4 flex justify-between items-center bg-blue-800">
                     <h1 className="text-white">DecExpress</h1>
                 </div>
-                <ul className="flex-1">{children}</ul>
+                <ul className="flex-1">{props.children}</ul>
             </nav>
         </aside>
     )
