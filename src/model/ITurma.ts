ed042e55ae5aba0fcbb6cea3cbb63d4481328fcc
Renata Albuquerque <renataalbq1@gmail.com>
2023-10-07 23:22:55 -0300

    add optional properties

--- src/model/ITurma.ts
+++ src/model/ITurma.ts
@@ -1,10 +1,10 @@
 import { IAluno } from "./IAluno";
 
 export type ITurma = {
-    codTurma: number;
+    codTurma?: number;
     ano: number;
     turma: string;
     serie: string;
     nivel: string
-    listaAlunos: Array<IAluno>;
+    listaAlunos?: Array<IAluno>;
 }
